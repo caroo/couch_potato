46dc01114016fa06a5a49397ca09d95cae22794d
Florian Frank <flori@ping.de>
2011-03-30 11:54:36 +0200

    Avoid loading unconfigured couch potata

--- rails/init.rb
+++ rails/init.rb
@@ -1,7 +1,10 @@
 # this is for rails only
 
-require File.dirname(__FILE__) + '/../lib/couch_potato'
+config_file_path = "#{Rails.root}/config/couchdb.yml"
+if File.exist?(config_file_path)
+  require File.dirname(__FILE__) + '/../lib/couch_potato'
 
-CouchPotato::Config.database_name = YAML::load(File.read(Rails.root.to_s + '/config/couchdb.yml'))[RAILS_ENV]
+  CouchPotato::Config.database_name = YAML::load(File.read(config_file_path))[RAILS_ENV]
 
-RAILS_DEFAULT_LOGGER.info "** couch_potato: initialized from #{__FILE__}"
+  RAILS_DEFAULT_LOGGER.info "** couch_potato: initialized from #{__FILE__}"
+end
